f2706280c0771265417615a1a1dd180fa3080247
Sana Ajani <sanaajani@msn.com>
2018-08-29 16:32:30 -0700

    async fetch tasks into stripped array

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,31 @@
 'use strict';
+
+
 // The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
 import * as $ from 'jquery';
 
-export function activate(context: vscode.ExtensionContext) {
+class StrippedTask {
+	constructor(public _type: String, public _script: String){
+	};
+}
+
+export async function activate(context: vscode.ExtensionContext) {
 
     console.log('Congratulations, your extension "taskrunnercode" is now active!');
+	
+    let tasks = await vscode.tasks.fetchTasks().then(function(value) {
+		return value;
+	});
 
-    let config = vscode.workspace.getConfiguration('tasks', vscode.window.activeTextEditor.document.uri);
-    let type = config.get("command");
-    let values = config.get("tasks");
- 
-    let taskNames = [];
+	let iterableTasks: StrippedTask[] = [];
 
-    for (var i=0; i < Object.keys(values).length; i++) {
-        taskNames[i] = values[i].taskName;
-    }
+	for (var i = 0; i < 2; i++) {
+		iterableTasks[i] = new StrippedTask(tasks[i].definition.type, tasks[i].definition.script);
+	}
 
-    vscode.window.showInformationMessage(`The tasks are ${taskNames} `);
-
-    // context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
-
+	
 }
-
